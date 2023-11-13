4d4a7a4b689e61aa1b162b002cd9e42ddfff211b
conkobar <connorkleine@gmail.com>
2023-11-13 14:10:12 -0600

    added auth api integration

--- my-app/src/routes/auth/Authentication.js
+++ my-app/src/routes/auth/Authentication.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './auth.css';
 import Login from './Login';
 import Register from './Register';
+import axios from 'axios';
 
 const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
   const [_switch, setSwitch] = useState(true);
@@ -16,9 +17,36 @@ const Authentication = ({ setIsLoggedIn, setUserUsername }) => {
     setSwitch(false);
   };
 
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (_switch) {
+      axios
+        .post('/api/auth/login', { username, password })
+        .then((response) => {
+          localStorage.setItem('token', response.data.token);
+          setUserUsername(username);
+          setIsLoggedIn(true);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    } else {
+      axios
+        .post('/api/auth/register', { username, password })
+        .then((response) => {
+          localStorage.setItem('token', response.data.token);
+          setUserUsername(username);
+          setIsLoggedIn(true);
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    }
+  };
+
   return (
     <div>
-      <form>
+      <form onSubmit={handleSubmit}>
         <button onClick={handleSignIn}>Sign In</button>
         <button onClick={handleSignUp}>Sign Up</button>
       </form>
